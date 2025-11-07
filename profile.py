503444b53fa664599f97e8ad65c7630057399184
mrarik7 <arikff007@gmail.com>
2025-11-07 16:14:06 +0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-st.header('👤Tarek Amin Arik')
-st.subheader('Tarek amin arik')
+st.header('ABOUT INFO')
+
 col1, col2 = st.columns(2)
 
 with (col1):
+    st.subheader('👤Tarek amin arik')
     st.image('https://scontent-bom5-1.xx.fbcdn.net/v/t1.15752-9/577110451_827647506755756_538703046407658389_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=0024fc&_nc_ohc=z57WWoi9Ey8Q7kNvwG6JBjc&_nc_oc=AdmR5GIA5YrMgjIlWj1jN7fCZxfVyngQZ-wRxgu7T5VREZQoGlBxsOAny-TGOpc91qOoq3zrL3H9_Ar_YHlYeVcu&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-bom5-1.xx&oh=03_Q7cD3wGcDSaR7gzJOZzugeoN6e2E7mgU4V85_JDhqrk8qN9J_w&oe=69342F51',width=200)
     st.text('-arik')
 with col2:
@@ -26,6 +27,7 @@ with col2:
     if s4:
         if x == s4:
             st.markdown('[✈️telegram:tarek amin arik](http://t.me/mr_arik7)')
+
 
 
 
